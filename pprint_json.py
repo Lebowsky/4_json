f527a6b3ac719e7b4388ace71c6a28fe10ae43d1
Andrey Lebowski <andrey.lebowski@gmail.com>
2017-12-23 19:52:59 +0300

    реализованы функции программы

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,18 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as json_data:
+        return json_data.read()    
 
 
 def pretty_print_json(data):
-    pass
+    try:
+        json_data = json.loads(data)
+    except ValueError as e:
+        print("Не удалось прочитать JSON-файл", e)
+        return
+    print (json.dumps(json_data, ensure_ascii = False, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data('example.json'))
