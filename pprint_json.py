f54d0be2665506f2ba19d5a2051711cc5a5e8339
Andrey Lebowski <andrey.lebowski@gmail.com>
2017-12-24 05:03:29 +0300

    описал изменения коммитом выше

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,20 @@
-import json
+import json, sys
 
 
 def load_data(filepath):
     with open(filepath, encoding='utf-8') as json_data:
-        return json_data.read()    
+        try:
+            json_data = json.load(json_data)
+        except ValueError as e:
+            print("Не удалось прочитать JSON-файл", e)
+            return
+        return json_data
 
 
-def pretty_print_json(text_json):
-    try:
-        json_data = json.loads(text_json)
-    except ValueError as e:
-        print("Не удалось прочитать JSON-файл", e)
-        return
+def pretty_print_json(json_data):
     print (json.dumps(json_data, ensure_ascii = False, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data('example.json'))
+    if len(sys.argv) > 1:
+        pretty_print_json(load_data(sys.argv[1]))
